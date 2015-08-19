04f8199bb9f2ca85cb57d0806dd2a3ab9a57836e
Chris Vannoy <chris.vannoy@theironyard.com>
2015-08-19 12:25:04 -0400

    Adds example stripe checkout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :user_sessions, :only => [:new, :create, :destroy]
   resources :taggings, :only => [:create, :destroy]
+  resources :charges, :only => [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
